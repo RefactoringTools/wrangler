21cbb7e02d10d71012ed8414a3e294ee6cdf943d
huiqing <hl@kent.ac.uk>
2008-09-01 11:35:26 +0000

    bug fixes; added annotation of process names

--- erl/refac_annotate_pid.erl
+++ erl/refac_annotate_pid.erl
@@ -38,7 +38,8 @@ ann_pid_info(DirList) ->
     Pid = start_fun_typesig_process([]),            %% Refactor this USING WRANGLER:  register a process, and remove the uses of Pid.
     SortedFuns1 = do_ann_pid_info(SortedFuns, Pid),
     stop_counter_process(),
-    lists:foreach(fun (File) -> {File, update_function(File, SortedFuns1)} end, Files).
+    lists:foreach(fun (File) -> {File, update_function(File, SortedFuns1)} end, Files),
+    ok.
     
 
 do_ann_pid_info(Funs, Pid) ->
@@ -51,7 +52,7 @@ bottom_up_ann(Funs, TypeSigPid) ->
 
 bottom_up_ann_1({{ModName, FunName, Arity}, FunDef}, TypeSigPid) ->
     EnvPid = start_env_process(),
-    {FunDef1, _} = annotate_special_fun_apps({{ModName, FunName, Arity}, FunDef}, EnvPid),
+    FunDef1 = annotate_special_fun_apps({{ModName, FunName, Arity}, FunDef}, EnvPid),
     FunDef2 = refac_util:full_buTP(fun annotate_within_fun/2, FunDef1, {ModName, FunName, Arity, EnvPid, TypeSigPid}),
     EnvPid ! stop, 
     {{ModName, FunName, Arity}, FunDef2}.
@@ -106,6 +107,7 @@ update_function(File, FunList) ->
 		end
 	end,
     {AnnAST1, _} = refac_util:stop_tdTP(F, AnnAST, []),
+    %%io:format("AnnAST1:\n~p\n", [AnnAST1]),
     refac_ast_server:update_ast(File, {AnnAST1, Info, filelib:last_modified(File)}),
     ok.
 
@@ -125,15 +127,25 @@ annotate_within_fun(Node, {_ModName, FunName, Arity, EnvPid, TypeSigPid}) ->
 	  end;
       application ->
  	    Operator = refac_syntax:application_operator(Node),
+	    Args = refac_syntax:application_arguments(Node),
  	    Ann = refac_syntax:get_ann(Operator),
  	    case lists:keysearch(fun_def, 1, Ann) of 
  		{value, {fun_def, {M1, F1, A1, _P1, _2}}} ->
  		   TypeSigPid ! {self(), get, {M1, F1, A1}},
  		   receive 
- 		       {TypeSigPid, value, {_ParSig,  RtnSig}} ->
- 			   case RtnSig of 
- 			       any -> Node;
- 			       Pid -> refac_util:update_ann(Node, Pid)
+ 		       {TypeSigPid, value, {ParSig,  RtnSig}} ->
+			   F= fun({A, S}) ->
+				      case S of
+					  {pname, _} ->    %% Can you do this to Pid?
+					      refac_util:update_ann(A, S);
+					  _ -> A
+				      end
+			      end,
+			   Args1 = lists:map(fun({A, S}) -> F({A,S}) end, lists:zip(Args, ParSig)),
+			   Node1 = refac_syntax:copy_attrs(Node, refac_syntax:application(Operator, Args1)),
+			   case RtnSig of 
+ 			       any -> Node1;
+ 			       Pid -> refac_util:update_ann(Node1, Pid)
  			   end;
  		       {TypeSigPid, false} ->
  			   Node
@@ -144,7 +156,7 @@ annotate_within_fun(Node, {_ModName, FunName, Arity, EnvPid, TypeSigPid}) ->
  	  P = refac_syntax:match_expr_pattern(Node),
  	  B = refac_syntax:match_expr_body(Node),
  	  Ann = refac_syntax:get_ann(B),
- 	  case lists:keysearch(pid, 1, Ann) of           %% Ingnore process name first.
+ 	  case lists:keysearch(pid, 1, Ann) of          
  	    {value, {pid, Value}} ->
  		P1 = refac_util:update_ann(P, {pid, Value}),
  		case refac_syntax:type(P) of
@@ -155,7 +167,21 @@ annotate_within_fun(Node, {_ModName, FunName, Arity, EnvPid, TypeSigPid}) ->
  		  _ -> ok                %%% What about the complex pattern matches?
  		end,
  		refac_syntax:copy_attrs(Node, refac_syntax:match_expr(P1, B));
- 	    _ -> Node %% The body is not a pid
+ 	    _ ->
+		  case lists:keysearch(pname, 1, Ann) of          
+		      {value, {pname, Value}} ->
+			  P1 = refac_util:update_ann(P, {pname, Value}),
+			  case refac_syntax:type(P) of
+			      variable ->
+				  Ann1 = refac_syntax:get_ann(P),
+				  {value, {def, DefinePos}} = lists:keysearch(def, 1, Ann1),
+				  EnvPid ! {add, {{def, DefinePos}, {pname, Value}}};
+			      _ -> ok                %%% What about the complex pattern matches?
+			  end,
+			  refac_syntax:copy_attrs(Node, refac_syntax:match_expr(P1, B));
+		      _ -> 
+			  Node %% The body is not a pid
+		  end
  	  end;
        function ->
  	    Ann = refac_syntax:get_ann(Node),
@@ -168,18 +194,25 @@ annotate_within_fun(Node, {_ModName, FunName, Arity, EnvPid, TypeSigPid}) ->
  		    LastExp = lists:last(refac_syntax:clause_body(C)),
  		    Res = case refac_syntax:type(LastExp) of
  			      match_expr -> refac_syntax:match_expr_pattern(LastExp);
- 			      _ -> LastExp
- 			  end,
- 		    ResInfo = case lists:keysearch(pid, 1, refac_syntax:get_ann(Res)) of
+ 			      _ -> LastExp  
+			  end,
+ 		     ResInfo = case lists:keysearch(pid, 1, refac_syntax:get_ann(Res)) of
  				  {value, {pid, Value}} -> {pid, Value};
- 				  _ -> any
- 			      end,
+				  _ -> case lists:keysearch(pname, 1, refac_syntax:get_ann(Res)) of 
+					   {value, {pname, Value}} -> {pname, Value};
+					   _ -> any
+				       end
+			       end,
 		    F = fun({P, _T}) ->
 				Ann1 = refac_syntax:get_ann(P), 
 				case lists:keysearch(pid,1,Ann1) of 
 				    {value, {pid, Value1}} ->
 					{pid, Value1};
-				    _ -> any
+				    _ -> case lists:keysearch(pname,1, Ann1) of 
+					     {value, {pname, V}} ->
+						 {pname, V};
+					     _-> any
+					 end
 				end
 			end,
  		    TypeSigPid ! {self(), get, {Mod, FunName, Arity}},		  	   
@@ -210,14 +243,16 @@ prop_from_calls(FunDef,  TypeSigPid) ->
  		case lists:keysearch(pid, 1, Ann) of
  		  {value, {pid, Value}} ->
  		      case Typ of
- 			any -> {pid, Value};
- 			{pid, Vs} -> {pid,lists:usort(Value++Vs)}
+			  {pid, Vs} -> {pid,lists:usort(Value++Vs)};
+			  _ -> {pid, Value}		 		 
 		      end;
 		    false ->
 			case Typ of 
 			    any -> Typ;
 			    %% Special case: some application sites are deciable pids, some are undeciable.
-			    {pid, Vs} -> {pid, lists:usort([any|Vs])}  
+			    {pid, Vs} -> {pid, lists:usort([any|Vs])};
+			    {pname, V} -> {pname, V}
+
 			end
  		end
  	end,
@@ -292,7 +327,7 @@ annotate_within_fun_1({{ModName, FunName, Arity}, FunDef}, TypeSigPid) ->
     {{ModName, FunName, Arity},FunDef1}.
     
 start_counter_process() ->               
-    Pid = spawn(fun() -> counter_loop({1,1}) end),
+    Pid = spawn_link(fun() -> counter_loop({1,1}) end),
     register(counter1, Pid).            %% REFACTOR THIS USING WRANGLER: RENAME counter1 to counter.
 
 stop_counter_process() ->
@@ -318,11 +353,12 @@ counter_loop({Spawn, Self}) ->
 %%% TO think: any other problems/restrictions with thus fun?
 annotate_special_fun_apps({CurrentFun,FunDef}, EnvPid) ->
     init_counter(),
-    Res = refac_util:stop_tdTP(fun do_annotate_special_fun_apps/2, FunDef, {CurrentFun, EnvPid}),
-    Res.
+    {FunDef1, _} = refac_util:stop_tdTP(fun do_annotate_special_fun_apps_pid/2, FunDef, {CurrentFun, EnvPid}),
+    refac_util:full_buTP(fun do_annotate_special_fun_apps_pname/2, FunDef1, EnvPid).
+   
 
 
-do_annotate_special_fun_apps(Node, {CurrentFun, EnvPid}) ->
+do_annotate_special_fun_apps_pid(Node, {CurrentFun, EnvPid}) ->
     case refac_syntax:type(Node) of
 	application ->
 	    case is_spawn_app(Node) of   %% TODO:How about meta application of spawn?
@@ -330,10 +366,10 @@ do_annotate_special_fun_apps(Node, {CurrentFun, EnvPid}) ->
 		    Op = refac_syntax:application_operator(Node),
 		    Args = refac_syntax:application_arguments(Node),
 		    Args1 = case Args of 
-				     [A] -> {A1, _} = refac_util:stop_tdTP(fun do_annotate_special_fun_apps/2, A, {refac_prettypr:format(A), EnvPid}),
+				     [A] -> {A1,_} = refac_util:stop_tdTP(fun do_annotate_special_fun_apps_pid/2, A, {refac_prettypr:format(A), EnvPid}),
 					    [A1];
 				     [Node, A] -> 
-					 {A1, _} = refac_util:stop_tdTP(fun do_annotate_special_fun_apps/2, A, {refac_prettypr:format(A), EnvPid}),
+					 {A1, _} = refac_util:stop_tdTP(fun do_annotate_special_fun_apps_pid/2, A, {refac_prettypr:format(A), EnvPid}),
 					 [Node, A1];
 				     _ -> Args			    
 				 end,	    
@@ -345,31 +381,112 @@ do_annotate_special_fun_apps(Node, {CurrentFun, EnvPid}) ->
 		    Node2 = refac_util:update_ann(Node1,
 						  {pid, [{spawn, CurrentFun, N}]}),
 		    {Node2, true};
-		_ ->
-		    case is_self_app(Node) of
-			true ->
+		_ -> 
+		    Op = refac_syntax:application_operator(Node),
+		    OpAnn= refac_syntax:get_ann(Op),
+		    case lists:keysearch(fun_def, 1, OpAnn) of 
+			{value, {fun_def, {erlang, self, 0, _, _}}} ->
 			    Node1 = refac_util:update_ann(Node, {pid, [{self, CurrentFun}]}),
 			    {Node1, true};
-			_ ->
-			    {Node, false}
+			_->{Node, false}
 		    end
 	    end;
 	_ -> {Node, false}
     end.
 
 
-
-is_self_app(T) ->
-    case refac_syntax:type(T) of
-      application ->
-	  Operator = refac_syntax:application_operator(T),
-	  Ann = refac_syntax:get_ann(Operator),
-	  case lists:keysearch(fun_def, 1, Ann) of
-	    {value, {fun_def, {erlang, self, 0, _, _}}} -> true;
-	    _ -> false
-	  end;
-      _ -> false
+do_annotate_special_fun_apps_pname(Node, EnvPid) ->
+    case refac_syntax:type(Node) of
+	application ->
+	    Op = refac_syntax:application_operator(Node),
+	    OpAnn= refac_syntax:get_ann(Op),
+	    case lists:keysearch(fun_def, 1, OpAnn) of 
+		{value, {fun_def, {erlang, register, 2, _,_}}} ->
+		    [Arg1, Arg2] = refac_syntax:application_arguments(Node),
+		    PidInfo = case lists:keysearch(pid, 1, refac_syntax:get_ann(Arg2)) of
+				  {value, {pid, PidInfo1}} -> PidInfo1;
+				  _ -> []
+			      end,
+		    io:format("Arg2:\n~p\n", [Arg2]),
+		    io:format("PidInfo:\n~p\n", [PidInfo]),
+		    Arg11 = refac_util:update_ann(Arg1, {pname, PidInfo}), 
+		    Node1 = refac_syntax:copy_attrs(Node, refac_syntax:application(Op, [Arg11, Arg2])),
+		    case lists:keysearch(def,1, refac_syntax:get_ann(Arg1)) of 
+			{value, {def, DefinePos}} ->  %% the process name is a variable.
+			    EnvPid ! {add, {{def, DefinePos}, {pname, PidInfo}}};
+			_ -> case refac_syntax:type(Arg1) of
+				 atom -> EnvPid ! {add, {{name, refac_syntax:atom_value(Arg1)}, {pname, PidInfo}}};
+				 _ -> ok
+			    end				 
+		    end,
+		    Node1;
+		{value, {fun_def, {erlang, unregister, 1, _,_}}} ->
+		    [Arg1] = refac_syntax:application_arguments(Node),
+		    case lists:keysearch(def,1, refac_syntax:get_ann(Arg1)) of 
+			{value, {def, DefinePos}} ->
+			    EnvPid !{self(), get, {def, DefinePos}},
+			    receive
+				{EnvPid, value, Value} ->
+				    Arg11 = refac_util:update_ann(Arg1, Value),  
+				    refac_syntax:copy_attrs(Node, refac_syntax:application(Op, [Arg11]));
+				{EnvPid, false} ->
+				    Arg11 = refac_util:update_ann(Arg1,{pname, []}), %% keep an empty list for information extension.
+				    EnvPid ! {add, {{def, DefinePos}, {pname, []}}},
+				    refac_syntax:copy_attrs(Node, refac_syntax:application(Op, [Arg11]))
+			    end;
+			_ -> Arg11 = refac_util:update_ann(Arg1,{pname, []}),
+			    refac_syntax:copy_attrs(Node, refac_syntax:application(Op, [Arg11]))
+		    end;
+		{value, {fun_def, {erlang, whereis, 1, _,_}}} ->
+		    [Arg1] = refac_syntax:application_arguments(Node),
+		    case lists:keysearch(def,1, refac_syntax:get_ann(Arg1)) of 
+			{value, {def, DefinePos}} ->
+			    EnvPid !{self(), get, {def, DefinePos}},
+			    receive
+				{EnvPid, value, Value} ->
+				    Arg11 = refac_util:update_ann(Arg1, Value),  
+				    refac_syntax:copy_attrs(Node, refac_syntax:application(Op, [Arg11]));
+				{EnvPid, false} ->
+				    Arg11 = refac_util:update_ann(Arg1,{pname, []}),
+				    EnvPid ! {add, {{def, DefinePos}, {pname, []}}},
+				    refac_syntax:copy_attrs(Node, refac_syntax:application(Op, [Arg11]))
+			    end;
+			_ -> 
+			    Arg11 = refac_util:update_ann(Arg1,{pname, []}),
+			    refac_syntax:copy_attrs(Node, refac_syntax:application(Op, [Arg11]))
+		    end;
+		{value, {fun_def, {erlang, send, 2, _, _}}} -> 
+		    [Arg1, Arg2] = refac_syntax:application_arguments(Node),
+		    Arg11 = case refac_syntax:type(Arg1) of 
+				atom -> refac_util:update_ann(Arg1, {pname, []});
+				_ -> Arg1
+			    end,
+		    Node1 = refac_syntax:copy_attrs(Node, refac_syntax:application(Op, [Arg11, Arg2])),
+		    case lists:keysearch(def,1, refac_syntax:get_ann(Arg1)) of 
+			{value, {def, DefinePos}} ->
+			    EnvPid ! {add, {{def, DefinePos}, {pname, []}}};
+			_ -> ok
+		    end,
+		    Node1; 
+		_ -> 		    
+		   Node
+	    end;
+	infix_expr -> Op  = refac_syntax:infix_expr_operator(Node),
+		      case refac_syntax:operator_name(Op) of 
+			  '!' ->
+			      Left = refac_syntax:infix_expr_left(Node),
+			      Right = refac_syntax:infix_expr_right(Node),
+			      case refac_syntax:type(Left) of 
+				  atom ->
+				      Left1 = refac_util:update_ann(Left, {pname, []}),
+				      refac_syntax:copy_attrs(Node, refac_syntax:infix_expr(Left1, Op, Right));
+				  _ -> Node
+			      end;
+			  _ -> Node
+		      end;
+	_ -> Node
     end.
+
 
 is_spawn_app(Tree) ->
     SpawnFuns1 = [{erlang, spawn, 1}, {erlang, spawn, 2}, {erlang, spawn, 3}, {erlang, spawn, 4},
