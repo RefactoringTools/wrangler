4d745d901026cf3b0c6c6b74daf62ddce4fd404e
huiqing <hl@kent.ac.uk>
2008-09-22 13:59:05 +0000

    added licence info

--- erl/wrangler_undo_server.erl
+++ erl/wrangler_undo_server.erl
@@ -1,9 +1,22 @@
 %%%-------------------------------------------------------------------
 %%% File    : wrangler_undo_server.erl
 %%% Author  :  <Huiqing>
-%%% Description : 
-%%%
-%%% Created : 31 Aug 2008 by  <>
+%%
+%% Copyright (C) 2006-2009  Huiqing Li, Simon Thompson
+
+
+%% The contents of this file are subject to the Erlang Public License,
+%% Version 1.1, (the "License"); you may not use this file except in
+%% compliance with the License. You should have received a copy of the
+%% Erlang Public License along with this software. If not, it can be
+%% retrieved via the world wide web at http://www.erlang.org/.
+
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%% the License for the specific language governing rights and limitations
+%% under the License.
+
+%% Author contact: hl@kent.ac.uk, sjt@kent.ac.uk
 %%%-------------------------------------------------------------------
 -module(wrangler_undo_server).
 
