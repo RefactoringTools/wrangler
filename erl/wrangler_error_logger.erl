dcb104950450aacf80dbae9aaafb5c31fedc7309
huiqing <hl@kent.ac.uk>
2009-01-05 13:05:40 +0000

    set trap_exit to true.

--- erl/wrangler_error_logger.erl
+++ erl/wrangler_error_logger.erl
@@ -48,6 +48,7 @@ add_error_to_logger(Error) ->
 %% Description: Initiates the server
 %%--------------------------------------------------------------------
 init([]) ->
+    process_flag(trap_exit, true),
     {ok, #state{}}.
 
 %%--------------------------------------------------------------------
@@ -69,8 +70,8 @@ handle_call(get_errors, _From, _State=#state{errors=Errors}) ->
 %%                                      {stop, Reason, State}
 %% Description: Handling cast messages
 %%--------------------------------------------------------------------
-handle_cast({add, Error}, _State=#state{errors=Errors}) ->
-    {noreply, #state{errors=lists:usort([Error|Errors])}}.
+handle_cast({add, {FileName, Error}}, _State=#state{errors=Errors}) ->
+    {noreply, #state{errors=([{FileName, Error}|lists:keydelete(FileName, 1, Errors)])}}.
 
 %%--------------------------------------------------------------------
 %% Function: handle_info(Info, State) -> {noreply, State} |
