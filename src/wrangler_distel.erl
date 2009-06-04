3e8ac0483284c96d968fde822def5ad61429a676
huiqing <hl@kent.ac.uk>
2009-06-04 21:58:41 +0000

    changed error message

--- src/wrangler_distel.erl
+++ src/wrangler_distel.erl
@@ -222,7 +222,8 @@ check_wrangler_error_logger() ->
 	[] ->
 	     ok;
 	_ ->  ?wrangler_io("\n===============================WARNING===============================\n",[]),
-	      ?wrangler_io("There are errors in the program, and functions/attribute containing errors are not affected by refactoring.\n",[]),
+	      ?wrangler_io("There are syntax errors, or syntaxes that are not supported by Wrangler, in the program,"
+			   " and functions/attribute containing syntaxes are not affected by refactoring.\n",[]),
 	      Msg =lists:flatmap(fun({FileName, Errs}) ->
 				    Str =io_lib:format("File:\n ~p\n", [FileName]),
 				    Str1 = Str ++ io_lib:format("Error(s):\n",[]),
