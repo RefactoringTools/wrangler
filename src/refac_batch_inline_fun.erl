80f093a039d44aedec9f0b053468794ccd319021
huiqing <hl@kent.ac.uk>
2011-08-10 22:20:57 +0100

    further development; still not completed yet.

--- src/refac_batch_inline_fun.erl
+++ src/refac_batch_inline_fun.erl
@@ -13,21 +13,22 @@ input_par_prompts() ->
 %% This version only works if the expr appears only once in the function, and 
 %% the function consists of only a single function clause and also that the 
 %% ExprStr denotes a literal expression.
-composite_refac(_Args=#args{current_file_name=CurFile, user_inputs=[M,F,A],
+
+composite_refac(_Args=#args{current_file_name=CurFile, %% user_inputs=[M,F,A],
                             search_paths=SearchPaths}) ->
-    Apps=fun() ->
-                 ?FULL_TD_TU([?COLLECT(?T("F@(Args@@)"), 
-                                       wrangler_misc:get_pos(_This@),
-                                       api_refac:fun_define_info(F@)=={M,F,A})],
-                             [CurFile])
-         end,
-    [{while, Apps()/=[], 
-      [{unfold_fun_app, [CurFile,hd(Apps()), SearchPaths]}]}].
+    Cond=fun() ->
+                 collect_apps(CurFile, {test,t,0})/=[]
+         end, 
+    [{while, Cond,
+      [{unfold_fun_app, [CurFile,fun()->
+                                         Apps = collect_apps(CurFile, {test,t,0}),
+                                         hd(Apps)
+                                 end,
+                         SearchPaths]}]}].
 
-
-
-%% ?LET(Apps, ?FULL_TD_TU([?COLLECT(?T("F@(Args@@)"), 
-%%                                  wrangler_misc:get_pos(_This@),
-%%                                  fun_define_info(F@)=={M,F,A})],
-%%                        [CurFile]),
-%%      {while, Apps/=[], [{unfold_fun_app, [CurFile, hd(Apps), SearchPaths]}
+collect_apps(File, {M,F,A}) ->
+    ?FULL_TD_TU([?COLLECT(?T("F@(Args@@)"),
+                          element(1, api_refac:start_end_loc(_This@)),
+                          api_refac:fun_define_info(F@)=={M,F,A})],
+                [File]).
+   
