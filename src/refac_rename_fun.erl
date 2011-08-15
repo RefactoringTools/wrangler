d1fd1c0f55d68728fb51c7366ca8ceede0d0b84a
huiqing <hl@kent.ac.uk>
2011-08-15 22:20:15 +0100

    extended to cover the case for composite_emacs. (cherry picked from commit ad3f4b2778c18b1922b45500094cd30997fe66cd)

--- src/refac_rename_fun.erl
+++ src/refac_rename_fun.erl
@@ -211,8 +211,13 @@ rename_fun_0(FileName, {AnnAST, Info}, {Mod, OldFunNameAtom, Arity}, {DefinePos,
 		    HasWarningMsg = wrangler_atom_utils:has_warning_msg(Pid),
 		    wrangler_atom_utils:output_atom_warning_msg(Pid, not_renamed_warn_msg(OldFunNameAtom), renamed_warn_msg(OldFunNameAtom)),
 		    wrangler_atom_utils:stop_atom_process(Pid),
-		    wrangler_write_file:write_refactored_files([{{FileName, FileName}, AnnAST1}| Results],
-							       HasWarningMsg, Editor, TabWidth, Cmd)
+		    Res=wrangler_write_file:write_refactored_files([{{FileName, FileName}, AnnAST1}| Results],
+                                                                   HasWarningMsg, Editor, TabWidth, Cmd),
+                    case Editor of 
+                        composite_emacs ->
+                            {Res, {name_change, [{{Mod, OldFunNameAtom, Arity}, {Mod, NewNameAtom, Arity}}]}};
+                        _ -> Res
+                    end
 	    catch
 		throw:Err ->
 		    wrangler_atom_utils:stop_atom_process(Pid),
@@ -226,7 +231,7 @@ rename_fun_0(FileName, {AnnAST, Info}, {Mod, OldFunNameAtom, Arity}, {DefinePos,
                                                            HasWarningMsg, Editor, TabWidth, Cmd),
             case Editor of 
                 composite_emacs ->
-                    {Res, {name_change, [{{Mod, OldFunNameAtom, Arity}, {Mod, NewNameAtom, Arity}}]}};
+                    {Res, {name_change, {{Mod, OldFunNameAtom, Arity}, {Mod, NewNameAtom, Arity}}}};
                 _ ->
                     Res
             end
