14b4e63a75768b81ef29b5c2fffbeb18fb705639
Roberto Souto Maior <roberto.barrosfilho@gmail.com>
2014-07-09 14:25:02 +0100

    Organizing tests

--- src/evaluation/evaluator/Tests/definitions2.erl
+++
@@ -1,5 +0,0 @@
--module(definitions2).
--export([g/1]).
-
-g(0) -> 0;
-g(N) when N > 0 -> N + g(N-1).
--- src/evaluation/tests/definitions/definitions2.erl
+++ src/evaluation/tests/definitions/definitions2.erl
@@ -1,5 +1,5 @@
 -module(definitions2).
--export([bar/1, strangeGuard/1]).
+-export([bar/1]).
 
 bar(N) when N > 0 -> N + bar(N-1);
 bar(0) -> 0.
