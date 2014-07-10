fc87a920354bd614a59af78b172a1fbf7e6d3fba
huiqing <h.li@kent.ac.uk>
2014-07-10 17:44:40 +0100

    updated the use of wrangler_scan:string.

--- src/refac_duplicated_code.erl
+++ src/refac_duplicated_code.erl
@@ -424,7 +424,7 @@ num_of_tokens(Exprs) ->
 
 
 num_of_tokens_in_string(Str) ->
-    case wrangler_scan:string(Str, {1,1}, 8, 'unix') of
+    case wrangler_scan:string_without_layout(Str, {1,1}) of
 	{ok, Ts, _} -> 
 	    length(Ts);
 	_ ->
