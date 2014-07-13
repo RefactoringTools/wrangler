a7727efb17679b843cab437b47344f9408b7bad4
Roberto Souto Maior <roberto.barrosfilho@gmail.com>
2014-07-13 18:22:27 +0100

    Adding tests/transformed/tests_funApp_arit_if file

---
+++ src/evaluation/tests/transformed/tests_funApp_arit_if.erl
@@ -0,0 +1,81 @@
+-module(tests_funApp_arit_if).
+-compile([export_all]).
+
+is_even(N) when is_number(N) ->
+    N rem 2 == 0.
+
+test1() -> [2].
+
+test2() ->
+    A = 1,
+    A.
+
+test3() -> [].
+
+test4() ->
+    X = true,
+    [2].
+
+test5() ->
+    X = false,
+    [].
+
+test6() ->
+    X = false,
+    Y = true,
+    [3].
+
+test7() ->
+    X = 4,
+    [2].
+
+test8() ->
+    Y = 4,
+    X = Y > 2,
+    [2].
+
+test9() -> [3].
+
+test10() ->
+    Y = 1,
+    X = Y > 2,
+    [3].
+
+test11(X) ->
+    if
+	X -> [2];
+	true -> []
+    end.
+
+test12() ->
+    Aux = 0 == 0,
+    [2].
+
+test13() ->
+    Aux = 1 == 0,
+    [].
+
+test14()->
+    if
+	2 < 1 -> 42;
+	false -> 37
+    end.
+	    
+funIf(X) ->
+    if
+	X > 2 -> [2];
+	true -> [3]
+    end.
+
+testFunIf() -> [2].
+ 	    	     
+%%can be done in 2 stages
+testCallTest4() ->
+    X = true,
+    if X -> [2];
+       true -> []
+    end.
+
+
+
+
