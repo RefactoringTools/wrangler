aa2330a8d2b6748ab55ed6079609d1875d32eaae
Pablo Lamela Seijas <P.Lamela-Seijas@kent.ac.uk>
2017-08-05 14:30:29 +0100

    Redo behaviour extraction example with size 7

--- examples/behaviour_extraction/ets_dets/automatic_output/common.erl
+++ examples/behaviour_extraction/ets_dets/automatic_output/common.erl
@@ -3,16 +3,10 @@
 -module(common).
 
 -export([any_exist/3, behaviour_info/1, common_1/3, common_10/3,
-         common_11/3, common_12/1, common_13/2, common_14/2, common_15/3,
-         common_16/2, common_17/1, common_18/1, common_19/0, common_2/1,
-         common_20/1, common_21/1, common_22/5, common_23/7, common_3/1,
-         common_4/2, common_5/4, common_6/4, common_7/3, common_8/3, common_9/1,
-         get_contents/2, init_state/2, initial_state/0, initial_state_data/0,
-         model_delete/2, model_insert/4, module_name/1, object/0, pattern/0,
-         precondition/5, prop_parallel/1, table_name/1, weight/3]).
-
--include_lib("eqc/include/eqc.hrl").
--include_lib("eqc/include/eqc_fsm.hrl").
+         common_11/5, common_12/5, common_2/2, common_3/3, common_4/2,
+         common_5/2, common_6/2, common_7/4, common_8/3, common_9/1,
+         init_state/2, model_delete/2, model_insert/4, object/1, pattern/1,
+         precondition/5, prop_parallel/1, weight/3]).
 
 -ifndef(BUG_INSERT_NEW).
 -define(BUG_INSERT_NEW,true).
@@ -21,26 +15,27 @@
 -record(state,{name,type,contents=[]}).
 
 behaviour_info(callbacks) ->
-    [{any_exist, 2}, {callback_1, 1}, {callback_10, 1}, {callback_11, 1},
-     {callback_12, 1}, {callback_13, 0}, {callback_14, 0}, {callback_15, 0},
-     {callback_16, 1}, {callback_17, 0}, {callback_18, 0}, {callback_19, 0},
-     {callback_2, 1}, {callback_20, 0}, {callback_21, 1}, {callback_22, 0},
-     {callback_23, 2}, {callback_24, 0}, {callback_25, 1}, {callback_26, 0},
-     {callback_27, 0}, {callback_28, 0}, {callback_29, 0}, {callback_3, 0},
-     {callback_30, 0}, {callback_31, 0}, {callback_32, 0}, {callback_33, 0},
-     {callback_34, 0}, {callback_35, 0}, {callback_36, 2}, {callback_37, 1},
-     {callback_38, 0}, {callback_39, 2}, {callback_4, 0}, {callback_40, 3},
-     {callback_5, 0}, {callback_6, 0}, {callback_7, 0}, {callback_8, 1},
-     {callback_9, 2}, {file_delete, 1}, {model_delete, 2}, {model_insert, 3},
-     {module_name, 0}, {object, 0}, {table_name, 0}];
+    [{aggregate, 2}, {any_exist, 2}, {callback_1, 0}, {callback_10, 0},
+     {callback_11, 0}, {callback_12, 0}, {callback_13, 1}, {callback_14, 2},
+     {callback_2, 0}, {callback_3, 1}, {callback_4, 0}, {callback_5, 0},
+     {callback_6, 0}, {callback_7, 0}, {callback_8, 0}, {callback_9, 1},
+     {collect, 2}, {command_names, 1}, {commands, 1}, {fails, 1},
+     {file_delete, 1}, {list, 1}, {model_delete, 2}, {model_insert, 3},
+     {module_name, 0}, {more_commands, 2}, {nat, 0}, {object, 0}, {oneof, 1},
+     {parallel_commands, 1}, {table_name, 0}];
 behaviour_info(_Other) -> undefined.
 
-common_1(Module, Objs, S) ->
-    S#state{contents = Module:model_insert(S#state.type,S#state.contents,Objs)}.
+common_1(S, T, V) -> S#state{name = V,type = T}.
 
-common_2(Res) -> Res == ok orelse Res == {error,not_owner}.
+common_2(Res, S) -> lists:sort(Res) == lists:sort(S#state.contents).
 
-common_3(Res) -> Res == ok.
+common_3(Module, Objs, S) ->
+    case Module:any_exist(Objs,S#state.contents) of
+        true ->
+            S;
+        false ->
+            S#state{contents = Module:model_insert(S#state.type,S#state.contents,Objs)}
+    end.
 
 common_4(Module, Name) ->
     case file:delete(Name) of
@@ -50,46 +45,36 @@ common_4(Module, Name) ->
             Module:file_delete(Name)
     end.
 
-common_5(Module, S, T, V) ->
-    S#state{name = Module:callback_1(V),type = T}.
+common_5(Module, S) ->
+    [{init_state,{call,Module:module_name(),Module:callback_2(),[Module:table_name()]}},
+     {opened,{call,Module:callback_6(),Module:callback_7(),[Module:table_name(),Module:callback_3(S)]}},
+   {opened,{call,Module:module_name(),Module:callback_8(),[Module:table_name(),Module:oneof([Module:object(),Module:list(Module:object())])]}},
+   {opened,{call,Module:module_name(),Module:callback_1(),[Module:table_name(),Module:callback_5()]}}
+     | Module:callback_4()].
 
-common_6(Module, Objs, Res, S) ->
-    Module:callback_2(Res) == not Module:any_exist(Objs,S#state.contents).
-
-common_7(Module, Objs, S) ->
-    S#state{contents = Module:model_insert(S#state.type,S#state.contents,Objs)}.
-
-module_name(Module) -> Module:callback_3().
-
-table_name(Module) -> Module:callback_4().
+common_6(Module, Cmds) ->
+    ?FORALL(Cmds,(Module:more_commands(3,Module:commands(Module))),
+            ?TRAPEXIT(Module:callback_9(Cmds))).
 
 %% Definition of the states. Each state is represented by a function, 
 %% listing the transitions from that state, together with generators 
 %% for the calls to make each transition.
 init_state(Module,_S) ->
-    [{opened,{call,Module:callback_6(),Module:callback_7(),[Module:table_name(),Module:callback_5()]}}
+    [{opened,{call,Module:callback_10(),Module:callback_11(),[Module:table_name(),Module:callback_12()]}}
      ].
 
-object() ->
-    {nat(),nat()}.
+object(Module) ->
+    {Module:nat(),Module:nat()}.
 
-pattern() ->
-    {oneof(['$1',nat()]),oneof(['$1','$2',nat()])}.
-
-%% Identify the initial state
-initial_state() ->
-    init_state.
-
-%% Initialize the state data
-initial_state_data() ->
-    #state{}.
+pattern(Module) ->
+    {Module:oneof(['$1',Module:nat()]),Module:oneof(['$1','$2',Module:nat()])}.
 
 %% Precondition (for state data).
 %% Precondition is checked before command is added to the command sequence
 precondition(Module,From,_To,S,{call,_,open_file,[_,[{type,T}]]}) ->
     lists:member(S#state.type,[undefined,T])
        andalso
-      Module:callback_8(From);
+      Module:callback_13(From);
 precondition(_Module,_From,_To,_S,{call,_,insert_new,_}) ->
     ?BUG_INSERT_NEW;
 precondition(_Module,_From,_To,_S,{call,_,_,_}) ->
@@ -116,16 +101,14 @@ model_delete(S,K) ->
 	  K1/=K].
 
 prop_parallel(Module) ->
-    fails(
-    %% Number of attempts to make each test case fail. When searching
-    %% for a failing example, we run each test once. When searching for
-    %% a way to shrink a test case, we run each candidate shrinking
-    %% 100 times.
-    ?FORALL(Attempts,?SHRINK(1,[100]),
-      ?FORALL({Seq,Par}, (parallel_commands(Module)),
-	?ALWAYS(Attempts,
-	  ?TIMEOUT(1000,(Module:callback_9(Par, Seq))))))
-     ).
+    Module:fails(%% Number of attempts to make each test case fail. When searching
+                 %% for a failing example, we run each test once. When searching for
+                 %% a way to shrink a test case, we run each candidate shrinking
+                 %% 100 times.
+                 ?FORALL(Attempts, ?SHRINK(1,[100]),
+                   ?FORALL({Seq,Par}, (Module:parallel_commands(Module)),
+                     ?ALWAYS(Attempts,
+	               ?TIMEOUT(1000,(Module:callback_14(Par, Seq))))))).
 
 
 %% Weight for transition (this callback is optional).
@@ -133,55 +116,28 @@ prop_parallel(Module) ->
 weight(_From,_To,{call,_,_,_}) ->
     1.
 
-get_contents(Module,Name) -> Module:callback_10(Name).
+common_7(Module, Objs, Res, S) ->
+    Res == not Module:any_exist(Objs,S#state.contents).
 
-common_8(Module, Objs, S) -> Module:any_exist(Objs,S#state.contents).
+common_8(Module, Objs, S) ->
+    S#state{contents = Module:model_insert(S#state.type,S#state.contents,Objs)}.
 
-common_9(Res) -> Res == ok.
+common_9(Res) -> Res == ok orelse Res == {error,not_owner}.
 
-common_10(Module, Res, S) ->
-    lists:sort(Module:callback_11(Res)) == lists:sort(S#state.contents).
-
-common_11(Module, K, S) ->
+common_10(Module, K, S) ->
     S#state{contents = Module:model_delete(S#state.contents,K)}.
 
-common_12(Module) ->
-    ?FORALL(Cmds,(more_commands(3,commands(Module))),
-            ?TRAPEXIT(Module:callback_12(Cmds))).
+common_11(Module, Cmds, H, Res, S) ->
+    ?WHENFAIL((io:format("History: ~p\nState: ~p\nRes: ~p\n",[H,S,Res])),
+              (Module:aggregate(Module:command_names(Cmds),
+                                Res == ok))).
 
-common_13(Module, S) ->
-    [{init_state,{call,Module:module_name(),Module:callback_17(),[Module:table_name()]}},
-     {opened,{call,Module:callback_14(),Module:callback_15(),[Module:table_name(),Module:callback_16(S)]}},
-   {opened,{call,Module:module_name(),Module:callback_18(),[Module:table_name(),oneof([Module:object(),list(Module:object())])]}},
-   {opened,{call,Module:module_name(),Module:callback_13(),[Module:table_name(),Module:callback_19()]}}
-     | Module:callback_20()].
-
-common_14(Module, Res) ->
-    Module:callback_21(Res) == Module:callback_22().
-
-common_15(Module, From, Res) ->
-    Module:callback_23(From, Res) == Module:callback_24().
-
-common_16(Module, Res) ->
-    Module:callback_25(Res) == Module:callback_26().
-
-common_17(Module) -> [{Module:callback_27(), Module:callback_28()}].
-
-common_18(Module) -> [Module:callback_30(), Module:callback_29()].
-
-common_19() -> oneof([set,bag]).
-
-common_20(Module) ->
-    {call,Module:callback_31(),Module:callback_32(),Module:callback_33()
-                                                   }.
-
-common_21(Module) -> [Module:callback_35(), Module:callback_34()].
-
-common_22(Module, H, ParH, Res, S) ->
-    [H,Module:callback_36(ParH, S) | Module:callback_37(Res)].
-
-common_23(Module, Cmds, H, Res, S, Par, ParH) ->
-    ?WHENFAIL((io:format(Module:callback_38(),[H,Module:callback_39(S,
-                                                                    ParH),Res])),(Module:callback_40(Cmds,
-                                                                                                     Res,
-                                                                                                     Par))).
+common_12(Module, H, Par, ParH, Res) ->
+    ?WHENFAIL((io:format("History: ~p\nParallel: ~p\n\nRes: ~p\n",
+                         [H,ParH,Res])),
+              (Module:collect(length(Par),
+                              Module:aggregate([length(P) || P <- Par],
+                                               Module:collect(length([ok
+                                                                      || P <- Par,
+                                                                         {set,_,{call,_,open_file,_}} <- P]),
+                                                      Res == ok))))).
--- examples/behaviour_extraction/ets_dets/automatic_output/dets_eqc.erl
+++ examples/behaviour_extraction/ets_dets/automatic_output/dets_eqc.erl
@@ -17,36 +17,36 @@
 
 -behaviour(common).
 
-module_name() -> common:module_name(?MODULE).
+module_name() -> dets.
 
-table_name() -> common:table_name(?MODULE).
+table_name() -> dets_table.
 
 init_state(S) -> common:init_state(?MODULE, S).
 
-opened(S) -> common:common_13(?MODULE, S).
+opened(S) -> common:common_5(?MODULE, S).
 
-object() -> common:object().
+object() -> common:object(?MODULE).
 
-pattern() -> common:pattern().
+pattern() -> common:pattern(?MODULE).
 
-initial_state() -> common:initial_state().
+%% Identify the initial state
+initial_state() ->
+    init_state.
 
-initial_state_data() -> common:initial_state_data().
+%% Initialize the state data
+initial_state_data() ->
+    #state{}.
 
 %% Next state transformation for state data.
 %% S is the current state, From and To are state names
 next_state_data(_From,_To,S,V,{call,_,open_file,[_,[{type,T}]]}) ->
-    common:common_5(?MODULE, S, T, V);
+    common:common_1(S, T, V);
 next_state_data(_From,_To,S,_V,{call,_,insert,[_,Objs]}) ->
-    common:common_7(?MODULE, Objs, S);
+    common:common_8(?MODULE, Objs, S);
 next_state_data(_From,_To,S,_V,{call,_,insert_new,[_,Objs]}) ->
-    case common:common_8(?MODULE, Objs, S) of
-	true ->
-	    S;
-	false -> common:common_1(?MODULE, Objs, S)
-    end;
+    common:common_3(?MODULE, Objs, S);
 next_state_data(_From,_To,S,_V,{call,_,delete,[_,K]}) ->
-    common:common_11(?MODULE, K, S);
+    common:common_10(?MODULE, K, S);
 next_state_data(_From,_To,S,_V,{call,_,_,_}) ->
     S.
 
@@ -56,15 +56,15 @@ precondition(From, To, S, X4) ->
 %% Postcondition, checked after command has been evaluated
 %% OBS: S is the state before next_state_data(From,To,S,_,<command>) 
 postcondition(_From,_To,_S,{call,_,close,_},Res) ->
-    common:common_2(Res);  %% not_owner in parallel tests
+    common:common_9(Res);  %% not_owner in parallel tests
 postcondition(_From,_To,_S,{call,_,delete,_},Res) ->
-    common:common_9(Res);
+    Res==ok;
 postcondition(_From,_To,_S,{call,_,insert,_},Res) ->
-    common:common_3(Res);
+    Res==ok;
 postcondition(_From,_To,S,{call,_,insert_new,[_,Objs]},Res) ->
-    common:common_6(?MODULE, Objs, Res, S);
+    common:common_7(?MODULE, Objs, Res, S);
 postcondition(_From,_To,S,{call,_,get_contents,_},Res) ->
-    common:common_10(?MODULE, Res, S);
+    common:common_2(Res, S);
 postcondition(_From,_To,_S,{call,_,_,_},_Res) ->
     true.
 
@@ -78,7 +78,7 @@ model_delete(S, K) -> common:model_delete(S, K).
 
 %% Top level property
 
-prop_dets() -> common:common_12(?MODULE).
+prop_dets() -> common:common_6(?MODULE, Cmds).
 
 %% Parallel testing property
 
@@ -92,113 +92,51 @@ open_file(Name,Args) ->
 
 %% Operations for inclusion in test cases
 
-get_contents(Name) -> common:get_contents(?MODULE, Name).
+get_contents(Name) ->
+    dets:traverse(Name,fun(X)->{continue,X}end).
 			       
 file_delete(Name) -> common:common_4(?MODULE, Name).
 
 
 
-callback_1(V) -> V.
+callback_1() -> insert_new.
 
-callback_2(Res) -> Res.
+callback_2() -> close.
 
-callback_3() -> dets.
+callback_3(S) -> [{type,S#state.type}].
 
-callback_4() -> dets_table.
+callback_4() ->
+    [{opened,{call,module_name(),delete,[table_name(),nat()]}},
+     {opened,{call,?MODULE,get_contents,[table_name()]}}
+      ].
 
-callback_5() -> common:common_17(?MODULE).
+callback_5() -> oneof([object(),list(object())]).
 
 callback_6() -> ?MODULE.
 
 callback_7() -> open_file.
 
-callback_8(From) ->
-    common:common_15(?MODULE, From, none) orelse ?BUG_OPEN_FILE.
+callback_8() -> insert.
 
-callback_9(Par, Seq) ->
+callback_9(Cmds) ->
+    dets:close(table_name()),
+    file_delete(table_name()),
+    {H,S,Res} = run_commands(?MODULE,Cmds),
+    common:common_11(?MODULE, Cmds, H, Res, S).
+
+callback_10() -> ?MODULE.
+
+callback_11() -> open_file.
+
+callback_12() -> [{type,oneof([set,bag])}].
+
+callback_13(From) -> From == init_state orelse ?BUG_OPEN_FILE.
+
+callback_14(Par, Seq) ->
     %% Make sure the table is not left open by a previous test.
     [dets:close(table_name()) || _ <- "abcdefghijkl"],
     %% Make sure the table is initially empty.
     file_delete(table_name()),
     %% Run the test case.
     {H,ParH,Res} = run_parallel_commands(?MODULE,{Seq,Par}),
-    ?WHENFAIL((io:format("History: ~p\nParallel: ~p\n\nRes: ~p\n",common:common_22(?MODULE,
-                                                                                   H,
-                                                                                   ParH,
-                                                                                   Res,
-                                                                                   none))),
-              (collect(length(Par),
-                       aggregate([length(P) || P <- Par],
-                                 collect(length([ok
-                                                 || P <- Par,
-                                                    {set,_,{call,_,open_file,_}} <- P]),common:common_16(?MODULE,
-                                                                                                         Res)))))).
-
-callback_10(Name) -> dets:traverse(Name,fun (X) -> {continue,X} end).
-
-callback_11(Res) -> Res.
-
-callback_12(Cmds) ->
-    dets:close(table_name()),
-    file_delete(table_name()),
-    {H,S,Res} = run_commands(?MODULE,Cmds),
-    common:common_23(?MODULE, Cmds, H, Res, S, none, none).
-
-callback_13() -> insert_new.
-
-callback_14() -> ?MODULE.
-
-callback_15() -> open_file.
-
-callback_16(S) -> [{type,S#state.type}].
-
-callback_17() -> close.
-
-callback_18() -> insert.
-
-callback_19() -> oneof([object(),list(object())]).
-
-callback_20() -> common:common_18(?MODULE).
-
-callback_21(Res) -> Res.
-
-callback_22() -> ok.
-
-callback_23(From, _Res) -> From.
-
-callback_24() -> init_state.
-
-callback_25(Res) -> Res.
-
-callback_26() -> ok.
-
-callback_27() -> type.
-
-callback_28() -> common:common_19().
-
-callback_29() -> {opened,{call,?MODULE,get_contents,[table_name()]}}.
-
-callback_30() ->
-    {opened,common:common_20(?MODULE)
-                                                            }.
-
-callback_31() -> module_name().
-
-callback_32() -> delete.
-
-callback_33() -> common:common_21(?MODULE).
-
-callback_34() -> nat().
-
-callback_35() -> table_name().
-
-callback_36(ParH, _S) -> ParH.
-
-callback_37(Res) -> [Res].
-
-callback_38() -> "History: ~p\nState: ~p\nRes: ~p\n".
-
-callback_39(S, _ParH) -> S.
-
-callback_40(Cmds, Res, _Par) ->
-    aggregate(command_names(Cmds),common:common_14(?MODULE, Res)).
+    common:common_12(?MODULE, H, Par, ParH, Res).
